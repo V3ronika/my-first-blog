17dd5a1961b0b5e0f0b1382aec1dc174d89b7a50
Veronika Smidova <veronika.smidova@gmail.com>
2017-03-12 17:28:23 +0100

    css, detail

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Post
 # Create your views here.
-def post_list(request): #input from user in 'request'
-    return render(request, 'blog/post_list.html', {}) #hlavna stranka na zobrazenie
+def post_list(request):#input from user in 'request', zobrazi sa ked natiahneme hlavnu stranku
+    posts = Post.objects.filter(published_date__lte=timezone.now()) #zoznam postov, ktore maju published date (maju sa publikovat) lte = less than or equal
+    return render(request, 'blog/post_list.html', {'posts':posts}) #hlavna stranka na zobrazenie
+
+def post_detail(request, pk):#input from user in 'request', zobrazi sa ked natiahneme hlavnu stranku
+    post = Post.objects.get(pk=pk) #primarny kluc
+    return render(request, 'blog/post_detail.html', {'post':post})
