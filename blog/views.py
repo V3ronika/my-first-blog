43b0e5cfadac24ac96f1493d766091b57af1a5db
Veronika Smidova <veronika.smidova@gmail.com>
2017-03-12 15:03:08 +0100

    fungujuce html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request): #input from user in 'request'
+    return render(request, 'blog/post_list.html', {}) #hlavna stranka na zobrazenie
